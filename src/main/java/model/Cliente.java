b64eca2993853aacace8bfc87e64f2174742a2b5
Diego Rodrigues <rdcodigo@gmail.com>
2023-03-10 13:42:39 -0300

    add Model Endereco

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,10 +1,12 @@
 package model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Cliente {
     @Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
